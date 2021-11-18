ac7ceafdee70a1e92c3f68a6510f38138c32ec27
Francisco <fcisco.dev@gmail.com>
2021-11-18 21:57:39 +0700

    adding UserRequest, SkillsetRequest, ExperienceRequest struct on user/presentation/request/dto

--- features/user/presentation/request/dto.go
+++ features/user/presentation/request/dto.go
@@ -1,14 +1,28 @@
 package request
 
-import "time"
+import (
+	"time"
+)
 
 type UserRequest struct {
-	Name        string    `json: "name"`
-	Address     string    `json: "address"`
-	Dob         time.Time `json: "dob"`
-	Gender      string    `json: "gender"`
-	Bio         string    `json: "bio"`
-	Title       string    `json: "title"`
-	Skillsets   []string  `json: "skillsets"`
-	Experiences []string  `json: "experiences`
+	Name        string              `json: "name"`
+	Address     string              `json: "address"`
+	Dob         time.Time           `json: "dob"`
+	Gender      string              `json: "gender"`
+	Bio         string              `json: "bio"`
+	Title       string              `json: "title"`
+	Skillsets   []SkillsetRequest   `json: "skillsets"`
+	Experiences []ExperienceRequest `json: "experiences"`
+}
+
+type SkillsetRequest struct {
+	Name     string `json: "name"`
+	Category string `json: "category"`
+}
+
+type ExperienceRequest struct {
+	Title       string
+	Description string
+	StartDate   time.Time
+	EndDate     time.Time
 }
