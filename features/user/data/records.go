bd9446637cbed643a8fa16169fa0b4ab784f5440
Francisco <fcisco.dev@gmail.com>
2021-11-18 22:19:10 +0700

    adding User, Skillset, Experience struct on user/data/records.go

--- features/user/data/records.go
+++ features/user/data/records.go
@@ -8,10 +8,27 @@ import (
 
 type User struct {
 	gorm.Model
-	Name    string    `json: "name"`
-	Address string    `json: "address"`
-	Bio     string    `json: "name"`
-	dob     time.Time `json: "dob"`
-	Title   string    `json: "title"`
-	Gender  string    `json: "gender"`
+	Name        string
+	Dob         time.Time
+	Gender      string
+	Address     string
+	Title       string
+	Bio         string
+	Skillsets   []Skillset `gorm: "many2many:user_skillsets;"`
+	Experiences []Experience
+}
+
+type Skillset struct {
+	ID       uint
+	UserID   uint
+	Category uint
+}
+
+type Experience struct {
+	ID          uint
+	UserID      uint
+	Description string
+	Title       string
+	StartDate   time.Time
+	EndDate     time.Time
 }
