99966d1225dad5b57ca0c49772bf4d9f3219febb
Francisco <fcisco.dev@gmail.com>
2021-11-18 20:08:06 +0700

    changing InsertData parameter type to job.JobCore on job/data/mysql.go. Use converter function to insert job.JobCore to database

--- features/job/data/mysql.go
+++ features/job/data/mysql.go
@@ -2,7 +2,6 @@ package data
 
 import (
 	"workuo/features/job"
-	"workuo/features/job/presentation/request"
 
 	"gorm.io/gorm"
 )
@@ -15,21 +14,21 @@ func NewMysqlJobRepository(DB *gorm.DB) job.Data {
 	return &mysqlJobRepository{DB}
 }
 
-func (j *mysqlJobRepository) InsertData(data request.Job) error {
-	convertedRequirement := []Requirement{}
-	for _, req := range data.Requirements {
-		convertedRequirement = append(convertedRequirement, Requirement{
-			Description: req,
-		})
-	}
-	convertedData := Job{
-		Title:        data.Title,
-		Description:  data.Description,
-		Recruiter_id: data.RecruiterID,
-		Requirements: convertedRequirement,
-	}
-
-	result := j.DB.Create(&convertedData)
+func (j *mysqlJobRepository) InsertData(data job.JobCore) error {
+	// convertedRequirement := []Requirement{}
+	// for _, req := range data.Requirements {
+	// 	convertedRequirement = append(convertedRequirement, Requirement{
+	// 		Description: req,
+	// 	})
+	// }
+	// convertedData := Job{
+	// 	Title:        data.Title,
+	// 	Description:  data.Description,
+	// 	Recruiter_id: data.RecruiterID,
+	// 	Requirements: convertedRequirement,
+	// }
+	recordData := toRecordJob(data)
+	result := j.DB.Create(&recordData)
 	if result.Error != nil {
 		return result.Error
 	}
