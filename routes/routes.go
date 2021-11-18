ba1d2b428249d5009eec744393b534d870978b25
Francisco <fcisco.dev@gmail.com>
2021-11-18 11:19:38 +0700

    adding basic route to create job with post method

---
+++ routes/routes.go
@@ -0,0 +1,20 @@
+package routes
+
+import (
+	"workuo/factory"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func New() *echo.Echo {
+	presenter := factory.Init()
+
+	e := echo.New()
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: "method=${method}, uri=${uri}, status=${status}\n",
+	}))
+	e.POST("/jobs", presenter.JobPresentation.CreateJobPostHandler)
+
+	return e
+}
