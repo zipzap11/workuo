d00c41036c2d8112ea9dbc0a283c23c0894c444f
Francisco <fcisco.dev@gmail.com>
2021-11-19 10:13:18 +0700

    implementing new contract for GetData func in user/data/mysql.go

--- features/user/data/mysql.go
+++ features/user/data/mysql.go
@@ -26,3 +26,14 @@ func (mr *mysqlUserRepository) InsertData(data user.UserCore) error {
 
 	return nil
 }
+
+func (mr *mysqlUserRepository) GetData() ([]user.UserCore, error) {
+	var users []User
+
+	err := mr.DB.Find(&users).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return nil, nil
+}
