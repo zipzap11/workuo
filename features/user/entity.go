af15083676a94076a249a59bf274586524262fd0
Francisco <fcisco.dev@gmail.com>
2021-11-20 15:31:05 +0700

    add new func GetUserById in interface contract for service and repository layer

--- features/user/entity.go
+++ features/user/entity.go
@@ -35,11 +35,13 @@ type ExperienceCore struct {
 type Service interface {
 	RegisterUser(data UserCore) (err error)
 	GetAllUser() (users []UserCore, err error)
+	GetUserById() (user UserCore, err error)
 	LoginUser(data UserCore) (user UserCore, err error)
 }
 
 type Repository interface {
 	InsertData(data UserCore) (err error)
 	GetData() (user []UserCore, err error)
+	GetDataById(user UserCore, err error)
 	CheckUser(data UserCore) (user UserCore, err error)
 }
