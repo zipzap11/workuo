2bd09f272cf7aca4c2bd41340da85854254c0555
Francisco <fcisco.dev@gmail.com>
2021-11-20 11:06:29 +0700

    add new interface contract func for user login on Service and repository layer

--- features/user/entity.go
+++ features/user/entity.go
@@ -10,6 +10,8 @@ type UserCore struct {
 	Address     string
 	Title       string
 	Bio         string
+	Email       string
+	Password    string
 	Skillsets   []SkillsetCore
 	Experiences []ExperienceCore
 }
@@ -32,9 +34,11 @@ type ExperienceCore struct {
 type Service interface {
 	RegisterUser(data UserCore) (err error)
 	GetAllUser() (users []UserCore, err error)
+	LoginUser(data UserCore) (user UserCore, err error)
 }
 
 type Repository interface {
 	InsertData(data UserCore) (err error)
 	GetData() (user []UserCore, err error)
+	CheckData(data UserCore) (user UserCore, err error)
 }
