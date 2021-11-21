39fa343f9d1eed3419f8dac398151e6c7c901763
Francisco <fcisco.dev@gmail.com>
2021-11-21 21:20:56 +0700

    add email and password field in recruiter struct on records.go

--- features/recruiter/data/records.go
+++ features/recruiter/data/records.go
@@ -8,15 +8,19 @@ import (
 
 type Recruiter struct {
 	gorm.Model
-	Company string
-	Bio     string
-	Address string
+	Company  string
+	Bio      string
+	Address  string
+	Email    string
+	Password string
 }
 
 func FromCore(data recruiter.RecruiterCore) Recruiter {
 	return Recruiter{
-		Company: data.Company,
-		Bio:     data.Bio,
-		Address: data.Address,
+		Company:  data.Company,
+		Bio:      data.Bio,
+		Address:  data.Address,
+		Email:    data.Email,
+		Password: data.Password,
 	}
 }
