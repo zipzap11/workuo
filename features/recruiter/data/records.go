fb2310ab65739c2fdbb07c2fb00f6ce485707cf8
Francisco <fcisco.dev@gmail.com>
2021-11-21 16:36:02 +0700

    Add converter func from RecruiterCore to Recrutier on records.go

--- features/recruiter/data/records.go
+++ features/recruiter/data/records.go
@@ -1,10 +1,22 @@
 package data
 
-import "gorm.io/gorm"
+import (
+	"workuo/features/recruiter"
+
+	"gorm.io/gorm"
+)
 
 type Recruiter struct {
 	gorm.Model
 	Company string
 	Bio     string
 	Address string
+}
+
+func fromCore(data recruiter.RecruiterCore) Recruiter {
+	return Recruiter{
+		Company: data.Company,
+		Bio:     data.Bio,
+		Address: data.Address,
+	}
 }
