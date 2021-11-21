53def63eda7b23a2118b42066d4e85c4dc962bb1
Francisco <fcisco.dev@gmail.com>
2021-11-21 16:29:03 +0700

    Add new interface for service and business layer in recruiter domain

--- features/recruiter/entity.go
+++ features/recruiter/entity.go
@@ -6,3 +6,11 @@ type RecruiterCore struct {
 	Address string
 	Bio     string
 }
+
+type Service interface {
+	RegisterRecruiter(data RecruiterCore) error
+}
+
+type Repository interface {
+	CreateRecruiter(data RecruiterCore) error
+}
