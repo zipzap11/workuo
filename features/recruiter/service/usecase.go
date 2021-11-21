b31cb8915adbd9abdfe62dc2689777fa63c125d6
Francisco <fcisco.dev@gmail.com>
2021-11-21 16:49:33 +0700

    create new struct recruiterService and implement recruiter.Service interface function

---
+++ features/recruiter/service/usecase.go
@@ -0,0 +1,22 @@
+package service
+
+import (
+	"workuo/features/recruiter"
+)
+
+type recruiterService struct {
+	recruiterRepository recruiter.Repository
+}
+
+func NewRecruiterService(recruiterRepo recruiter.Repository) recruiter.Service {
+	return &recruiterService{recruiterRepo}
+}
+
+func (rs *recruiterService) RegisterRecruiter(data recruiter.RecruiterCore) error {
+	err := rs.recruiterRepository.CreateRecruiter(data)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
