fcf2b1b32b6391a583314ee2d3b0d5487f8a8182
Francisco <fcisco.dev@gmail.com>
2021-11-22 12:17:48 +0700

    modify GetData func on user repository layer, add join query to filter skillsets

--- features/user/data/mysql.go
+++ features/user/data/mysql.go
@@ -25,19 +25,6 @@ func (mr *mysqlUserRepository) InsertData(data user.UserCore) error {
 	return nil
 }
 
-func (mr *mysqlUserRepository) GetData(data user.UserCore) ([]user.UserCore, error) {
-	var users []User
-
-	queryStr := "%" + data.Title + "%"
-	err := mr.DB.Where("title like ?", queryStr).Preload("Skillsets").Preload("Experiences").Find(&users).Error
-
-	if err != nil {
-		return nil, err
-	}
-
-	return toUserCoreList(users), nil
-}
-
 func (mr *mysqlUserRepository) CheckUser(data user.UserCore) (user.UserCore, error) {
 	var userData User
 	err := mr.DB.Where("email = ? and password = ?", data.Email, data.Password).First(&userData).Error
@@ -66,10 +53,13 @@ func (mr *mysqlUserRepository) GetDataById(data user.UserCore) (user.UserCore, e
 	return toUserCore(userData), nil
 }
 
-func (mr *mysqlUserRepository) GetDataBySkillsets(data user.UserCore) ([]user.UserCore, error) {
+func (mr *mysqlUserRepository) GetData(data user.UserCore) ([]user.UserCore, error) {
 	var users []User
-	// queryStr := "%" + data.
-	err := mr.DB.Preload("Skillsets").Preload("Experiences").Joins("JOIN user_skillsets ON user_skillsets.user_id = user.id JOIN skillsets ON skillsets.id = user_skillsets.skillset_id AND skillsets LIKE = ?", ("%" + data.Skillsets[0].Name + "%")).Find(&users).Error
+
+	titleFilter := "%" + data.Title + "%"
+	err := mr.DB.Where("title LIKE ?", titleFilter).Preload("Skillsets").Preload("Experiences").
+		Joins("JOIN user_skillsets ON user_skillsets.user_id = users.id JOIN skillsets ON skillsets.id = user_skillsets.skillset_id AND skillsets.name LIKE ?", ("%" + data.Skillsets[0].Name + "%")).
+		Find(&users).Error
 	if err != nil {
 		return nil, err
 	}
