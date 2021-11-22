64c930b89db0f42bd1c18c6b0289072002520238
Francisco <fcisco.dev@gmail.com>
2021-11-22 10:01:06 +0700

    add new func to contract interface for get user by skillsets

--- features/user/entity.go
+++ features/user/entity.go
@@ -34,14 +34,16 @@ type ExperienceCore struct {
 
 type Service interface {
 	RegisterUser(data UserCore) (err error)
+	LoginUser(data UserCore) (user UserCore, err error)
 	GetUsers(data UserCore) (users []UserCore, err error)
 	GetUserById(data UserCore) (user UserCore, err error)
-	LoginUser(data UserCore) (user UserCore, err error)
+	GetUserBySkillsets(data UserCore) (users []UserCore, err error)
 }
 
 type Repository interface {
 	InsertData(data UserCore) (err error)
+	CheckUser(data UserCore) (user UserCore, err error)
 	GetData(UserCore) (user []UserCore, err error)
 	GetDataById(data UserCore) (user UserCore, err error)
-	CheckUser(data UserCore) (user UserCore, err error)
+	GetDataBySkillsets(data UserCore) (users []UserCore, err error)
 }
