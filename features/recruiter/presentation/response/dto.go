bcf307490af3dac790e8f8189e517eeed8f0dcfb
Francisco <fcisco.dev@gmail.com>
2021-11-23 12:03:31 +0700

    add new response struct and converter func

--- features/recruiter/presentation/response/dto.go
+++ features/recruiter/presentation/response/dto.go
@@ -1,12 +1,36 @@
 package response
 
-import "workuo/features/recruiter"
+import (
+	"net/http"
+	"workuo/features/recruiter"
+
+	"github.com/labstack/echo/v4"
+)
+
+type Response struct {
+	Message string      `json: "message"`
+	Data    interface{} `json: "data"`
+}
 
 type RecruiterLoginResponse struct {
 	ID      uint   `json: "id"`
 	Company string `json: "company"`
 	Address string `json: "address"`
 	Token   string `json: "token"`
+}
+
+type RecruiterResponse struct {
+	ID      uint   `json: "id"`
+	Company string `json: "company"`
+	Address string `json: "address"`
+	Bio     string `json: "bio`
+}
+
+func NewSuccessResponse(e echo.Context, msg string, data interface{}) error {
+	return e.JSON(http.StatusOK, Response{
+		Message: msg,
+		Data:    data,
+	})
 }
 
 func ToRecruiterLoginResponse(data recruiter.RecruiterCore) RecruiterLoginResponse {
@@ -16,4 +40,22 @@ func ToRecruiterLoginResponse(data recruiter.RecruiterCore) RecruiterLoginRespon
 		Address: data.Address,
 		Token:   data.Token,
 	}
+}
+
+func ToRecruiterResponse(data recruiter.RecruiterCore) RecruiterResponse {
+	return RecruiterResponse{
+		ID:      data.ID,
+		Company: data.Company,
+		Address: data.Address,
+		Bio:     data.Bio,
+	}
+}
+
+func ToRecruiterResponseList(data []recruiter.RecruiterCore) []RecruiterResponse {
+	convertedRec := []RecruiterResponse{}
+	for _, rec := range data {
+		convertedRec = append(convertedRec, ToRecruiterResponse(rec))
+	}
+
+	return convertedRec
 }
