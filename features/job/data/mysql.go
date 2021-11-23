5945139180648c9e151a741bf0d55253ef845399
Francisco <fcisco.dev@gmail.com>
2021-11-23 14:07:36 +0700

    implement func GetJobDataById

--- features/job/data/mysql.go
+++ features/job/data/mysql.go
@@ -33,3 +33,13 @@ func (jr *mysqlJobRepository) GetJobData(data job.JobCore) ([]job.JobCore, error
 
 	return toCoreList(jobs), nil
 }
+
+func (jr *mysqlJobRepository) GetJobDataById(data job.JobCore) (job.JobCore, error) {
+	var jobData Job
+	err := jr.DB.Preload("Requirements").Find(&jobData, data.ID).Error
+	if err != nil {
+		return job.JobCore{}, err
+	}
+
+	return jobData.toCore(), nil
+}
