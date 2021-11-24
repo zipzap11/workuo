209818a80f74234464a38a3e238b473f30000904
Francisco <fcisco.dev@gmail.com>
2021-11-24 15:11:49 +0700

    create struct for data model on db

---
+++ features/application/data/records.go
@@ -0,0 +1,35 @@
+package data
+
+import (
+	"time"
+	"workuo/features/application"
+
+	"gorm.io/gorm"
+)
+
+type Application struct {
+	gorm.Model
+	UserID    uint
+	JobID     uint
+	Status    string
+	AppliedAt time.Time
+}
+
+func ToApplicationRecord(data application.ApplicationCore) Application {
+	return Application{
+		UserID:    data.UserID,
+		JobID:     data.JobID,
+		Status:    data.Status,
+		AppliedAt: data.AppliedAt,
+	}
+}
+
+func ToCore(data Application) application.ApplicationCore {
+	return application.ApplicationCore{
+		ID:        data.ID,
+		UserID:    data.UserID,
+		JobID:     data.JobID,
+		Status:    data.Status,
+		AppliedAt: data.AppliedAt,
+	}
+}
