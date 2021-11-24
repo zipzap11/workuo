dfa453f9e7e0961bac22b43b2b91e70f235f75b5
Francisco <fcisco.dev@gmail.com>
2021-11-24 21:52:35 +0700

    create struct for handling request data

--- features/application/presentation/request/dto.go
+++ features/application/presentation/request/dto.go
@@ -1,1 +1,15 @@
 package request
+
+import "workuo/features/application"
+
+type ApplicationRequest struct {
+	JobID  uint `json: "JobID`
+	UserID uint `json: "UserID`
+}
+
+func (app *ApplicationRequest) ToCore() application.ApplicationCore {
+	return application.ApplicationCore{
+		UserID: app.UserID,
+		JobID:  app.JobID,
+	}
+}
