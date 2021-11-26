56224e621ad7b0331ab91d0501b64fa5857c3b0d
Francisco <fcisco.dev@gmail.com>
2021-11-27 01:17:13 +0700

    add RecruiterId, Role attribute on InvitationRequest

--- features/invitation/presentation/request/dto.go
+++ features/invitation/presentation/request/dto.go
@@ -3,13 +3,17 @@ package request
 import "workuo/features/invitation"
 
 type InvitationRequest struct {
-	UserID uint `json: "userid"`
-	JobID  uint `json: "jobid"`
+	UserID      uint `json: "userid"`
+	JobID       uint `json: "jobid"`
+	RecruiterID uint
+	Role        string
 }
 
 func ToCore(data InvitationRequest) invitation.InvitationCore {
 	return invitation.InvitationCore{
-		UserID: data.UserID,
-		JobID:  data.JobID,
+		UserID:      data.UserID,
+		JobID:       data.JobID,
+		RecruiterID: data.RecruiterID,
+		Role:        data.Role,
 	}
 }
