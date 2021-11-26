c48f517a8784675601c7501c3a9eb864d2706576
Francisco <fcisco.dev@gmail.com>
2021-11-26 11:15:30 +0700

    create InvitationRequest struct nad func ToCore

--- features/invitation/presentation/request/dto.go
+++ features/invitation/presentation/request/dto.go
@@ -1,1 +1,15 @@
 package request
+
+import "workuo/features/invitation"
+
+type InvitationRequest struct {
+	UserID uint `json: "userid"`
+	JobID  uint `json: "jobid"`
+}
+
+func ToCore(data InvitationRequest) invitation.InvitationCore {
+	return invitation.InvitationCore{
+		UserID: data.UserID,
+		JobID:  data.JobID,
+	}
+}
