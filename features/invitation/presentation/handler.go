bf0de5f950129528ee25786ffd4be1f9ff67751e
Francisco <fcisco.dev@gmail.com>
2021-11-26 11:14:44 +0700

    create invite user handler

--- features/invitation/presentation/handler.go
+++ features/invitation/presentation/handler.go
@@ -1,6 +1,13 @@
 package presentation
 
-import "workuo/features/invitation"
+import (
+	"net/http"
+	"workuo/features/invitation"
+	"workuo/features/invitation/presentation/request"
+	"workuo/features/invitation/presentation/response"
+
+	"github.com/labstack/echo/v4"
+)
 
 type InvitationHandler struct {
 	invService invitation.Service
@@ -8,4 +15,19 @@ type InvitationHandler struct {
 
 func NewInvitationHandler(is invitation.Service) *InvitationHandler {
 	return &InvitationHandler{is}
+}
+
+func (ih *InvitationHandler) InviteUserHandler(e echo.Context) error {
+	var payloadData request.InvitationRequest
+	err := e.Bind(&payloadData)
+	if err != nil {
+		return response.NewErrorResponse(e, http.StatusBadRequest, err.Error())
+	}
+
+	err = ih.invService.InviteUser(request.ToCore(payloadData))
+	if err != nil {
+		return response.NewErrorResponse(e, http.StatusInternalServerError, err.Error())
+	}
+
+	return response.NewSuccessResponse(e, nil)
 }
