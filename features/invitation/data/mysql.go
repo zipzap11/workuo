a02bca9eba089418035df6732e734b02a645451a
Francisco <fcisco.dev@gmail.com>
2021-11-26 11:12:37 +0700

    implement invite user on repository

--- features/invitation/data/mysql.go
+++ features/invitation/data/mysql.go
@@ -13,3 +13,13 @@ type invitationRepository struct {
 func NewInvitationRepository(DB *gorm.DB) invitation.Repository {
 	return &invitationRepository{DB}
 }
+
+func (ir *invitationRepository) InviteUser(data invitation.InvitationCore) error {
+	recordData := FromCore(data)
+	err := ir.DB.Create(&recordData).Error
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
