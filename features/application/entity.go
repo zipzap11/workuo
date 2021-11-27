31d73c8357d5507ff090664d2be4bda1764fa77e
Francisco <fcisco.dev@gmail.com>
2021-11-27 15:49:06 +0700

    add jobcore and requrirement core struct

--- features/application/entity.go
+++ features/application/entity.go
@@ -10,6 +10,24 @@ type ApplicationCore struct {
 	JobID     uint
 	Status    string
 	AppliedAt time.Time
+	Job       JobCore
+}
+
+type JobCore struct {
+	ID           int
+	Title        string
+	Description  string
+	RecruiterId  int
+	Company      string
+	Requirements []RequirementCore
+	Created_at   time.Time
+	Updated_at   time.Time
+}
+
+type RequirementCore struct {
+	ID          uint
+	JobId       uint
+	Description string
 }
 
 type Repository interface {
