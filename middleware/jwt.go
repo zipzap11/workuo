04263a762a606b939fe9f41a9931e2778766398a
Francisco <fcisco.dev@gmail.com>
2021-11-27 12:15:46 +0700

    create ExtracClaims function

--- middleware/jwt.go
+++ middleware/jwt.go
@@ -5,6 +5,7 @@ import (
 	"workuo/config"
 
 	"github.com/golang-jwt/jwt"
+	"github.com/labstack/echo/v4"
 )
 
 func CreateToken(userId uint, name string) (string, error) {
@@ -17,4 +18,14 @@ func CreateToken(userId uint, name string) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 
 	return token.SignedString([]byte(config.JWT_KEY))
+}
+
+func ExtractClaim(e echo.Context) (claims map[string]interface{}) {
+	user := e.Get("user").(*jwt.Token)
+
+	if user.Valid {
+		claims = user.Claims.(jwt.MapClaims)
+	}
+
+	return
 }
