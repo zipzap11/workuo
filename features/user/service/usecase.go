a039c1b949a48fc91d41623fbab8473ffe0dab4b
Francisco <fcisco.dev@gmail.com>
2021-11-29 20:10:03 +0700

    implement UpdateUser func on service layer

--- features/user/service/usecase.go
+++ features/user/service/usecase.go
@@ -55,3 +55,64 @@ func (us *userService) GetUserById(id int) (user.UserCore, error) {
 
 	return userData, nil
 }
+
+func (us *userService) UpdateUser(data user.UserCore) error {
+	// update primary user data
+	err := us.userRepository.UpdateUser(data)
+	if err != nil {
+		return err
+	}
+
+	// add or update or delete user-skillsets
+	for _, skill := range data.Skillsets {
+		if skill.Id == 0 {
+			skillId, err := us.userRepository.CreateSkillset(skill)
+			if err != nil {
+				return err
+			}
+
+			err = us.userRepository.AddUserSkillset(int(data.Id), skillId)
+			if err != nil {
+				return err
+			}
+		} else {
+			if skill.Name == "" {
+				err := us.userRepository.DeleteUserSkillset(int(data.Id), int(skill.Id))
+				if err != nil {
+					return err
+				}
+			} else {
+				skillId, err := us.userRepository.CreateSkillset(skill)
+				if err != nil {
+					return err
+				}
+				err = us.userRepository.UpdateUserSkillset(int(data.Id), int(skill.Id), skillId)
+				if err != nil {
+					return err
+				}
+			}
+		}
+	}
+
+	// update experiences
+	for _, exp := range data.Experiences {
+		if exp.Id == 0 {
+			exp.UserId = data.Id
+			err = us.userRepository.CreateExperience(exp)
+			if err != nil {
+				return err
+			}
+		} else {
+			if exp.Title == "" {
+				err = us.userRepository.DeleteExperience(int(exp.Id))
+			} else {
+				err = us.userRepository.UpdateExperience(exp)
+			}
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
