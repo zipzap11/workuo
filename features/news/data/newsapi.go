2855ad788a90450077bfacd15bd75fd642c08131
Francisco <fcisco.dev@gmail.com>
2021-11-30 03:29:50 +0700

    add newsApi for repository layer

---
+++ features/news/data/newsapi.go
@@ -0,0 +1,44 @@
+package data
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"workuo/features/news"
+)
+
+type NewsApi struct {
+	URL    string
+	Client http.Client
+	Key    string
+}
+
+func NewNewsApiRepository(url string, key string) news.Repository {
+	return &NewsApi{
+		URL:    url,
+		Client: http.Client{},
+		Key:    key,
+	}
+}
+
+func (nr *NewsApi) GetData() ([]news.NewsCore, error) {
+	url := fmt.Sprintf("%v?access_key=%v&languages=en", nr.URL, nr.Key)
+	request, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var newsData Data
+
+	response, err := nr.Client.Do(request)
+	if err != nil {
+		return nil, err
+	}
+
+	err = json.NewDecoder(response.Body).Decode(&newsData)
+	if err != nil {
+		return nil, err
+	}
+
+	return ToCoreList(newsData.Data), nil
+}
